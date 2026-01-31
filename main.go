f1b0aa98109a1bbdbc9d9537aabd6c32874da7d6
Bartosz Wrona <EasyRider190@gmail.com>
2026-01-31 23:26:10 +0100

    add map command

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	pokeapi "github.com/theadduser/pokego/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
